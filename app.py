a81fd652f18185ad237d59089a433bf046d88efa
valentim <j.pedro__@hotmail.com>
2026-03-12 20:57:40 -0300

    explicações  1 e 2
    adição de blocos de explicação - consultar livro e blog referencia com o professor.

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
 import streamlit as st;
 from pandas import pandas as pd
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+
+
+""" 
+Regressão Linear > É um modelo de aprendizado supervisionado, o que significa que ele aprendeu a 
+relação entre os dados históricos (diâmetro da pizza) e os resultados conhecidos (preço).
+"""
+
 
 df = pd.read_csv('pizzas.csv')
 
 modelo = LinearRegression()
 x =  df[['diametro']]
 y = df[['preco']]
+
+"""
+Para que ele serve?   A Regressão Linear serve para prever um valor numérico contínuo (regressão) 
+com base na relação linear entre variáveis.No nosso caso específico, o Objetivo seria: Prever o 
+preço (variável dependente y) baseado no tamanho (variável independente x ).
+
+Funcionamento: O algoritmo tenta traçar a "melhor linha reta" que passa entre os pontos de dados do 
+seu arquivo pizzas.csv. 
+Essa linha minimiza a distância entre ela e todos os pontos reais do gráfico.
+
+"""
+
+
 
 modelo.fit(x,y)
 
